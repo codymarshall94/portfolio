8900ff67bf75bea9fd39e2a9df755e24089b8567
Cody Marshall <codymstuntsx@gmail.com>
2022-05-31 14:26:31 -0700

    added styles to home container

---
+++ src/Components/HomeComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../css/homecomponent.css";
+
+function HomeComponent() {
+  return (
+    <div className="d-flex justify-content-center home-container">
+      <div className="border-gradient home-page-frame">
+        <div className="header-one">
+          <h1>Code-y Marshall</h1>
+        </div>
+        <div className="header-two">
+          <h2>Creates Stuff</h2>
+        </div>
+        <div className="header-three">
+            <span>Full Stack Web Developer</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeComponent;
