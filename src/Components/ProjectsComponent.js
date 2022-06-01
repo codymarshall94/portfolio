6cdb66bf52d7072e1a86168b5dce4351ef71433f
Cody Marshall <codymstuntsx@gmail.com>
2022-05-31 17:33:54 -0700

    styled the project item containers

--- src/Components/ProjectsComponent.js
+++ src/Components/ProjectsComponent.js
@@ -6,6 +6,54 @@ function ProjectsComponent() {
     <div id="projects" className="projects container">
       <div className="section-header">
         <h3>Projects</h3>
+        <div class="container">
+          <div class="row gx-5 mt-2">
+            <div class="col">
+              <div className="border-gradient project-item">
+              </div>
+              <div className="project-text">
+                <span className="project-text-color">Project One</span>
+              </div>
+            </div>
+            <div class="col">
+              <div className="border-gradient project-item">
+              </div>
+              <div className="project-text">
+                <span className="project-text-color">Project Two</span>
+              </div>
+            </div>
+            <div class="col">
+              <div className="border-gradient project-item">
+              </div>
+              <div className="project-text">
+                <span className="project-text-color">Project Three</span>
+              </div>
+            </div>
+          </div>
+          <div class="row gx-5 mt-2">
+            <div class="col">
+              <div className="border-gradient project-item">
+              </div>
+              <div className="project-text">
+                <span className="project-text-color">Project Four</span>
+              </div>
+            </div>
+            <div class="col">
+              <div className="border-gradient project-item">
+              </div>
+              <div className="project-text">
+                <span className="project-text-color">Project Five</span>
+              </div>
+            </div>
+            <div class="col">
+              <div className="border-gradient project-item">
+              </div>
+              <div className="project-text">
+                <span className="project-text-color">Project Six</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
