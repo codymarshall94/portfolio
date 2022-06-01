b32cab648785fab6b1d0debc286cd8d1371ba441
Cody Marshall <codymstuntsx@gmail.com>
2022-06-01 15:45:32 -0700

    added about and contact sections

---
+++ src/Components/AboutComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/aboutcomponent.css';
+
+function AboutComponent() {
+  return (
+    <div className='container about' id='about'>
+        <div className='section-header'>
+            <h3>About Me</h3>
+        </div>
+    </div>
+  )
+}
+
+export default AboutComponent
---
+++ src/Components/ContactComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../css/contactcomponent.css";
+
+function ContactComponent() {
+  return (
+    <div className="container contact d-flex justify-content-center flex-column" id="contact">
+      <div className="section-header">
+        <h3>Contact</h3>
+      </div>
+      <div className="container">
+          <div className="d-flex justify-content-center flex-column align-items-center mt-5">
+              <span>
+                  Still Learning But Im Ready!
+              </span>
+              <span>
+                  @codymarshall@gmail.com
+              </span>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactComponent;
