782644cc7273cac10cca86faef882007e8a4788d
Cody Marshall <codymstuntsx@gmail.com>
2022-06-06 13:47:31 -0700

    adjusted home layout

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -6,13 +6,13 @@ function HomeComponent() {
     <div className="d-flex justify-content-center home-container" id="home">
       <div className="border-gradient home-page-frame">
         <div className="header-one">
-          <h1>Code-y Marshall</h1>
+          <h1>Cody Marshall</h1>
         </div>
         <div className="header-two">
           <h2>Creates Stuff</h2>
         </div>
         <div className="header-three">
-            <span>Full Stack Web Developer</span>
+            <span>Apsiring Front End Developer</span>
         </div>
       </div>
     </div>
