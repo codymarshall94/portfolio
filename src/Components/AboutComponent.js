9ce992ed9fa4c9aa0d9913257abd892045b54396
Cody Marshall <codymstuntsx@gmail.com>
2022-06-06 17:29:21 -0700

    updated text positioning

--- src/Components/AboutComponent.js
+++ src/Components/AboutComponent.js
@@ -7,6 +7,12 @@ function AboutComponent() {
         <div className='section-header'>
             <h3>About Me</h3>
         </div>
+        <div className='mt-5'>
+          <p className='about-text'>
+          <span className='hey-text'>Hey,</span><br/>Years of professional Parkour to developing creative interactive websites.<br/>
+          When im not flipping off walls, I enjoy learning to develop with React and making things look visually appealing.
+          </p>
+        </div>
     </div>
   )
 }
--- src/Components/ContactComponent.js
+++ src/Components/ContactComponent.js
@@ -1,21 +1,34 @@
 import React from "react";
 import "../css/contactcomponent.css";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function ContactComponent() {
   return (
-    <div className="container contact d-flex justify-content-center flex-column" id="contact">
+    <div
+      className="container contact d-flex justify-content-center flex-column"
+      id="contact"
+    >
       <div className="section-header">
-        <h3>Contact</h3>
+        <h3>Lets Keep This Simple</h3>
       </div>
       <div className="container">
-          <div className="d-flex justify-content-center flex-column align-items-center mt-5">
-              <span className="gradient-text">
-                  Still Learning But Im Ready!
-              </span>
-              <span className="gradient-text">
-                  @codymarshall@gmail.com
-              </span>
-          </div>
+        <div className="d-flex justify-content-center flex-column align-items-center mt-5">
+          <span className="gradient-text">@codymarshall@gmail.com</span>
+        </div>
+        <div className="d-flex justify-content-center">
+          <a
+            className="nav-link social-icon px-1"
+            href="https://github.com/codymarshall94"
+          >
+            <AiFillGithub color="#3FBC9D" />
+          </a>
+          <a
+            className="nav-link social-icon px-1"
+            href="https://www.linkedin.com/in/cody-marshall-8aa8b315a/"
+          >
+            <AiFillLinkedin color="#ECE2BF" />
+          </a>
+        </div>
       </div>
     </div>
   );
