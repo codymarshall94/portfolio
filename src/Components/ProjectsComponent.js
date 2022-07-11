e6a7a99fe1bb22dda9f6e6fa6b92f12802f4f2d9
Cody Marshall <codymstuntsx@gmail.com>
2022-07-11 12:19:42 -0700

    added project image and in devlopment icons

--- src/Components/ProjectsComponent.js
+++ src/Components/ProjectsComponent.js
@@ -1,47 +1,65 @@
 import React from "react";
 import "../css/projectscomponent.css";
+import doubleup from "../images/doubleup.png";
+import { BsGear } from "react-icons/bs";
 
 function ProjectsComponent() {
   return (
     <div id="projects" className="projects container">
       <div className="section-header">
         <h3>Projects</h3>
-        <div class="container">
-          <div class="row mt-2">
-            <div class="col-sm-12 col-md-6 col-lg-4 project-item-container">
-              <div className="border-gradient project-item"></div>
+        <div className="container">
+          <div className="row mt-2">
+            <div className="col-sm-12 col-md-6 col-lg-4 project-item-container">
+              <div className="border-gradient project-item">
+                <img
+                  className="project-img"
+                  src={doubleup}
+                  alt="double up game screenshot"
+                />
+              </div>
               <div className="project-text">
-                <span className="project-text-color">Project One</span>
+                <span className="project-text-color">Double Up Game</span>
               </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 project-item-container">
-              <div className="border-gradient project-item"></div>
+            <div className="col-sm-12 col-md-6 col-lg-4 project-item-container">
+              <div className="border-gradient project-item">
+                <BsGear />
+              </div>
               <div className="project-text">
-                <span className="project-text-color">Project Two</span>
+                <span className="project-text-color">In Devlopment</span>
               </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 project-item-container">
-              <div className="border-gradient project-item"></div>
+            <div className="col-sm-12 col-md-6 col-lg-4 project-item-container">
+              <div className="border-gradient project-item">
+                <BsGear />
+              </div>
               <div className="project-text">
-                <span className="project-text-color">Project Three</span>
+                <span className="project-text-color">In Devlopment</span>
               </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 project-item-container">
-              <div className="border-gradient project-item"></div>
+            <div className="col-sm-12 col-md-6 col-lg-4 project-item-container">
+              <div className="border-gradient project-item">
+                <BsGear />
+              </div>
               <div className="project-text">
-                <span className="project-text-color">Project Four</span>
+                <span className="project-text-color">In Devlopment</span>
               </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 project-item-container">
-              <div className="border-gradient project-item"></div>
+            <div className="col-sm-12 col-md-6 col-lg-4 project-item-container">
+              <div className="border-gradient project-item">
+                <BsGear />
+              </div>
               <div className="project-text">
-                <span className="project-text-color">Project Five</span>
+                <span className="project-text-color">In Devlopment</span>
               </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 project-item-container">
-              <div className="border-gradient project-item"></div>
+            <div className="col-sm-12 col-md-6 col-lg-4 project-item-container">
+              <div className="border-gradient project-item">
+                <BsGear />
+              </div>
               <div className="project-text">
-                <span className="project-text-color">Project Six</span>
+                <span className="project-text-color">In Devlopment</span>
               </div>
             </div>
           </div>
